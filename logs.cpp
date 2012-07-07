7cc1129b7285ff799610e2a5b7f7cf731584dfff
Watcheur <moriceau.mathieu@epitech.eu>
2012-07-07 13:30:11 +0200

    Différence pot/prepot + boss

--- logs.cpp
+++ logs.cpp
@@ -2,6 +2,8 @@
 #include "window.h"
 #include "logs.h"
 
+int ji = 1;
+
 // 12/28 21:02:23.629  SPELL_AURA_APPLIED,0x0680000002F548FA,"Rhogar",0x511,0x0,0x0680000002F548FA,"Rhogar",0x511,0x0,79633,"Agilit des Tol'vir",0x10,BUFF
 
 // 79633 - Tol'vir - Agility
@@ -16,6 +18,7 @@ Logs::Logs(std::string filename)
     std::deque<std::string> args;
 
     this->on_boss = false;
+    this->actual_boss = "NO BOSS";
     this->filedes.open(filename.c_str(), std::ifstream::in);
     if (filedes.is_open())
     {
@@ -24,15 +27,17 @@ Logs::Logs(std::string filename)
             getline(filedes, line);
             args = parse_log(line);
             track_boss(args);
-            if (this->on_boss == true)
+            if (args[2] == "SPELL_AURA_APPLIED" && atoi(args[11].c_str()) == 110660)
             {
-                if (args[2] == "SPELL_AURA_APPLIED")
-                    track_potions(args);
+                this->actual_boss = "Echine d'Aile de Mort";
+                this->on_boss = true;
             }
+            if (args[2] == "SPELL_AURA_APPLIED")
+                track_potions(args);
         }
     }
     else
-        std::cout << "Unable to open file" << std::endl;
+        std::cout << "Unable to open file" << filename << std::endl;
 }
 
 Logs::~Logs()
@@ -50,22 +55,25 @@ void    Logs::track_boss(std::deque<std::string> args)
         {"NULL", "NULL", "Hagara la Lieuse des tempêtes", "Hagara the Stormbinder"},
         {"NULL", "NULL", "Ultraxion", "Ultraxion"},
         {"NULL", "NULL", "Maître de guerre Corne-Noire", "Warmaster Blackhorn"},
-        {"Tendons brûlants", "Burning Tendons", "Echinne d'Aile de Mort", "Spine of Deathwing"},
+        {"Tendons brûlants", "Burning Tendons", "Echine d'Aile de Mort", "Spine of Deathwing"},
         {"Tentacule d’aile", "Wing Tentacle", "Folie d'Aile de Mort", "Madness of Deathwing"},
     };
-    int i;
+    int i = 0;
 
     for (i = 0; i <= 7; ++i)
     {
-        if ((args[2].find("_DAMAGE") == std::string::npos) && (tab[i].specific_tracking_a == args[8] || tab[i].specific_tracking_b == args[8] || tab[i].name_fr == args[8] || tab[i].name_en == args[8]))
+        if (args.size() >= 9)
         {
-            this->on_boss = true;
-            this->actual_boss = tab[i].name_fr;
-        }
-        if ((args[2].find("UNIT_DIED") == std::string::npos) && (tab[i].specific_tracking_a == args[4] || tab[i].specific_tracking_b == args[4] || tab[i].name_fr == args[4] || tab[i].name_en == args[4]))
-        {
-            this->on_boss = false;
-            this->actual_boss = "not on boss";
+            if ((args[2].find("_DAMAGE") == std::string::npos) && (tab[i].specific_tracking_a == args[8] || tab[i].specific_tracking_b == args[8] || tab[i].name_fr == args[8] || tab[i].name_en == args[8]))
+            {
+                this->on_boss = true;
+                this->actual_boss = tab[i].name_fr;
+            }
+            if ((args[2].find("UNIT_DIED") == std::string::npos) && (tab[i].specific_tracking_a == args[4] || tab[i].specific_tracking_b == args[4] || tab[i].name_fr == args[4] || tab[i].name_en == args[4]))
+            {
+                this->on_boss = false;
+                this->actual_boss = "not on boss";
+            }
         }
     }
 }
@@ -117,8 +125,19 @@ void    Logs::track_potions(std::deque<std::string> args)
 
     while (idx < 5)
     {
-        if (my_potions[idx].item_id == atoi(args[11].c_str()) && this->on_boss == true)
-            std::cout << args[4] << " a utilisé une potion " << my_potions[idx].type << " sur le boss " << this->actual_boss << std::endl;
+        if (my_potions[idx].item_id == atoi(args[11].c_str()))
+        {
+            if (this->on_boss == true)
+            {
+                std::cout << ji << ": " << args[4] << " a utilisé une potion " << my_potions[idx].type << " sur le boss " << this->actual_boss << std::endl;
+                ji++;
+            }
+            else
+            {
+                std::cout << ji << ": " << args[4] << " a prepote " << my_potions[idx].type << std::endl;
+                ji++;
+            }
+        }
         idx++;
     }
 }
